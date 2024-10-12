25d689185ca970a5a5b2d57ef4143eb34b1a7345
hackcatml <hackcatml@hackcatml>
2024-10-12 23:49:45 +0900

    Upload a script that sets the watchpoint on all threads

---
+++ script_set_watchpoint_all_threads.js
@@ -0,0 +1,57 @@
+const base = Module.findBaseAddress('libUE4.so');
+const addCoin = base.add(0x69a4450);
+
+Interceptor.attach(addCoin, {
+    onEnter: function(args) {
+        this.instance = args[0];
+        console.log(`\n[*] AddCoin() is called, class instance is ${args[0]}`);
+    },
+    onLeave: function(ret) {
+        console.log(`[*] TotalCoins are stored at class instance + 0x310`);
+        console.log(hexdump(ptr(this.instance).add(0x310), {length: 16}));
+    }
+})
+
+let unsetWatchPoint = false;
+let _addr, _size, _conditions;
+let threads = null;
+function installWatchpoint(addr, size, conditions) {
+    _addr = addr;
+    _size = size;
+    _conditions = conditions;
+    threads = Process.enumerateThreads();
+    Process.setExceptionHandler(e => {
+        console.log(`\n[!] ${e.context.pc} tried to "${_conditions}" at ${_addr}`);
+        if (['breakpoint', 'single-step'].includes(e.type)) {
+            for (const thread of threads) {
+                if (thread.id === Process.getCurrentThreadId()) {
+                    thread.unsetHardwareWatchpoint(0);
+                    unsetWatchPoint = true;
+                    return true;
+                }
+            }
+        }      
+        return false;
+    });  
+    for (const thread of threads) {
+        try {
+            thread.setHardwareWatchpoint(0, addr, size, conditions);
+            console.log(`[*] HardwareWatchpoint set at ${addr} (${thread.id} ${thread.name})`);
+        } catch (error) {}
+    }
+}
+
+function reInstallWatchPoint() {
+    for (const thread of threads) {
+        try {
+            thread.setHardwareWatchpoint(0, _addr, _size, _conditions);
+        } catch (error) {}
+    }
+}
+
+var int = setInterval(() => {
+    if (unsetWatchPoint) {
+        reInstallWatchPoint();
+        unsetWatchPoint = false;
+    }
+}, 0);
